cc046b3eb08da12d1c1264b70168b56e7c89ba27
NajeebSultan <najeebsultan070@gmail.com>
2025-08-05 19:33:27 +0500

    Add routes in app.js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import './index.css'; // Assuming you have an index.css file for styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
