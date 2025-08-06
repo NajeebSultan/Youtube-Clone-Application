9888560aaf2057b2241e8983e064af27fc3fbe72
NajeebSultan <najeebsultan070@gmail.com>
2025-08-07 01:17:04 +0500

    Write name and give color to side bar menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Stack } from "@mui/material" 
+import { categories } from "../utils/constants" 
 
-const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+const selectedCategory = "New" // Default selected category
+
+const Sidebar = () => (
+  <Stack direction="row"
+    sx={{ overflowY: "auto",
+     height: { sx: "auto", md: "95%" }, 
+     flexDirection: { md: "column" } 
+     }}
+  >
+    {categories.map((category) => (
+       <button 
+       className="category-btn"
+       style={{
+        background: category.name === 
+        selectedCategory && '#FC1503',
+       }}
+       >
+        <span>{category.icon}</span>
+        <span>{category.name}</span>
+       </button>
+
+    ))}
+
+  </Stack>
+)
 
 export default Sidebar
