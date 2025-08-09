7cb182b5ebf61d4dae7ac6c00440ca643868ada9
NajeebSultan <najeebsultan070@gmail.com>
2025-08-10 02:08:22 +0500

    Edit video Details

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import { useState , useEffect} from 'react';
+import { link , useParams} from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Typography ,Box , Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import { Video } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
+  const [videoDetail , setVideoDetail] = useState(null);
+  const { id} = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`video?part=snippet,statistic&id=${id}`)
+    .then((data) => setVideoDetail(data.item[0]));
+  } , [id]);
+
+  const {snippet: { title , channelId , channelTitle }, statistics : {viewCount , likeCount} } = videoDetail;
+
   return (  
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column' , md: 'row'}}>
+        <Box flex={1}>
+          <Box sx={{ width: '100%' , position: 'sticky' , top: '86px'}}>
+
+            <ReactPlayer url={'https://www.youtube.com/watch?v=${id}'}
+            className = "react-player" controls/>
+            <Typography color="#fff" variant="h5" fontWeight = "bold" p= {2}>
+              {videoDetail.snippet.title}
+            </Typography>
+            <Stack direction="row" justifyContent="space-between" sx={{
+              color: '#fff'}} py={1} px ={2}>
+                
+
+            </Stack>
+          </Box>
+        </Box>
+
+      </Stack>
+    </Box>
   )
 }
 
