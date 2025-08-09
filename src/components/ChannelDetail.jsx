88b1908be6bd214f2f76aa601bc7ea260a31513a
NajeebSultan <najeebsultan070@gmail.com>
2025-08-10 00:09:02 +0500

    Give styling to channel card

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -2,10 +2,13 @@ import { useState , useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
-import { Videos, ChannelCard} from './';
+import { videos, ChannelCard} from './';
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
+ 
 const ChannelDetail = () => {
+const [ channelDetail , setChannelDetail] = useState(null) 
+const [ videos , setVideos] = useState([])
 
  const { id } = useParams();
  
@@ -22,11 +25,17 @@ const ChannelDetail = () => {
       <Box>
         <div style={{
           background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+          height: '300px',
 
         }}
-        />
-       </Box>
+ />
+        <ChannelCard channelDetail={channelDetail} />
+        
       </Box>
+       
+       
+    </Box>
   )
 }
 
