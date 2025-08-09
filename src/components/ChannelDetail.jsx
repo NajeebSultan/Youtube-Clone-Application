7cb617184fdcffbfe1009e45234ae94e680938f5
NajeebSultan <najeebsultan070@gmail.com>
2025-08-09 23:58:46 +0500

    Give styling to or box

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -18,7 +18,15 @@ const ChannelDetail = () => {
   .then((data) => setVideos(data?.items));
 } , [id]);
   return (  
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+
+        }}
+        />
+       </Box>
+      </Box>
   )
 }
 
